09ee774cc69a09a6701651a52a24ef1b4fb54908
maatheuss0 <82424913+maatheuss0@users.noreply.github.com>
2024-04-30 21:44:53 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,131 @@
+function substituirConteudo() {
+    var novoConteudo = `
+    <body>
+
+    <header
+        style="top: 0; left: 0; padding: 10px; z-index: 1000; background-image: url(./assets/zeca_wallpaper.jpg); background-size: cover; background-attachment: fixed; height: 500px;">
+    </header>
+
+    <section>
+        <div style="text-align: center;">
+            <div style="display: flex; justify-content: center;"> 
+                <h1
+                    style="margin-top: 50px ; font-size: 60px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
+                    Zeca Pagodinho</h1>
+                    <img style="width: 100px; height: 100px; margin-top:40px; margin-left: 10px;" src="./assets/samba.png">
+            </div>
+            <hr width="1000px">
+        </div>
+    </section>
+
+    <section>
+        <div style="margin-top: 100px ; display: flex; justify-content: center;">
+            <img style="width: 500px;" src="./assets/zeca4.jpg">
+            <div
+                style="width: 30%; height: 420px; ; margin-left: -50px; font-size: 20px; margin-top: 100px; font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; text-align: justify; background-color: #FFFFFF; padding: 30px; box-shadow: 0 5px 20px rgba(0, 0, 0, 0.1);">
+                <p>ㅤㅤJessé Gomes da Silva Filho, o Zeca Pagodinho, nasceu no dia 14 de fevereiro de 1959, em Irajá e foi
+                    criado em Del Castiho, bairros do subúrbio do Rio de Janeiro. Desde cedo, destacou-se nas rodas de
+                    samba
+                    que frequentava nos subúrbios cariocas, onde apresentou
+                    suas
+                    primeiras rimas e sua voz rouca e rasgada de partideiro. <br> 
+                    ㅤㅤFoi em 1981, na quadra do bloco carnavalesco Cacique de Ramos, onde se reuniam todas as
+                    quartas-feiras
+                    compositores e cantores num encontro denominado pagode, que Zeca conheceu a cantora Beth Carvalho
+                    que o
+                    convidou para participar de seu novo disco e cantar a faixa "Camarão que Dorme a Onda Leva", de sua
+                    autoria em parceria com Arlindo Cruz.</p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div style="text-align: center; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"" >
+            <div style="display: flex; justify-content: center;"> 
+                <h1
+                style="margin-top: 90px ; font-size: 40px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
+                Carreira</h1>
+            <img style="width: 70px; height: 70px; margin-top: 80px;" src="./assets/rj.png">
+            </div>
+            <hr width="600px">
+        </div>
+    </section>
+
+    <section>
+        <div class="area1" style="margin-top: 80px ; display: flex; justify-content: center;">
+            <img style="width: 500px;" src="./assets/zeca10.jpeg">
+            <div
+                style="width: 30%; height: 420px; ; margin-left: -50px; font-size: 20px; margin-top: 40px;font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; text-align: justify; background-color: #FFFFFF; padding: 30px; box-shadow: 0 5px 20px rgba(0, 0, 0, 0.1);">
+                <p>ㅤㅤEm 1986, Zeca Pagodinho gravou seu primeiro disco solo, “Zeca Pagodinho”, que fez sucesso com as músicas: “Judia de Mim” e “Coração em Desalinho” e “Brincadeira Tem Hora”. O LP é considerado uma obra-prima do partido-alto. A música “Judia de Mim” foi trilha sonora da novela “Hipertensão”. <br> 
+
+                ㅤㅤEm 1988, pela RCA, Zeca gravou “Jeito Moleque”, com diversas composições suas. Em seguida lançou: “Boêmio Feliz” (1989), “Mania da Gente” (1990), “Pixote” (1991), “Um dos Poetas do Samba” (1992) e “Alô, Mundo” (1993). <br> 
+                
+                ㅤㅤEm 1996 assinou com a Universal e no mesmo ano lançou o LP “Deixa Clarear”, que fez grande sucesso com a música título. <br> 
+                
+                ㅤㅤEm 1999 lançou o CD “Zeca Pagodinho ao Vivo”, que vendeu mais de meio milhão de cópias. No ano seguinte, lançou o disco “Água da Minha Sede”.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+    <div style="text-align: center; margin-top: 70px;" >
+        <div style="display: flex; justify-content: center;"> 
+            <h1
+                style="margin-top: 20px ; font-size: 40px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
+                Músicas</h1>
+                <img style="width: 70px; height: 70px; margin-top: 10px;margin-left: 20px;" src="./assets/cavaco.png">
+        </div>
+        <hr width="600px">
+        <div style="margin-top:20px ;display: flex; justify-content: center; align-items: center; flex-direction: column ;">            
+            <h2 class="elemento" style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"><a class="elemento" style="color:black;" href="https://www.youtube.com/watch?v=Nm5N4iFLU0g">1. Quando A Gira Girou</a></h2>
+            <h2 class="elemento" style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"><a class="elemento" style="color:black;" href="https://www.youtube.com/watch?v=fl4sSBjHQJ0">2. Minha fé</a></h2>
+            <h2 class="elemento" style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"><a class="elemento" style="color:black;" href="https://www.youtube.com/watch?v=oTREAvZbmME">3. Deixa a Vida Me Levar</a></h2>
+            <h2 class="elemento" style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"><a class="elemento" style="color:black;" href="https://www.youtube.com/watch?v=7YaHP3u4UK4">4 .Faixa Amarela</a></h2>
+        </div>
+    </div>
+    </section>
+
+    <section">
+        <div style="text-align: center; margin-top: 70px;" >
+            <div style="display: flex; justify-content: center;"> 
+                <h1
+                style="margin-top: 20px ; font-size: 40px; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
+                Curiosidades</h1>
+                <img style="width: 70px; height: 70px; margin-top: 10px;margin-left: 20px;" src="./assets/quest.png">
+            </div>
+            <hr width="600px">
+            <div style="display: flex; justify-content: center; align-items: center; flex-direction: column ;">
+                <h2 style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; margin-top: 40px;" >Zeca é aquariano.</h2>
+                <h2 style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" >Seu nome de batismo é Jessé Gomes da Silva Filho.</h2>
+                <h2 style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" >Em entrevista ao "De frente com Gabi", ele contou que já foi feirante, office-boy e anotador de jogo de bicho antes da fama.</h2>
+                <h2 style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" >Tem no peito uma tatuagem de São Cosme e Damião.</h2>
+                <h2 style="border: 2px solid #0000000f; background-color: rgb(255, 255, 255); width: 800px; border-radius: 6px; padding: 20px; box-shadow: 0 3px 8px -3px rgba(0,0,0,.2); font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" >Em 2003, gravou um Acústico MTV, sendo, até então, o segundo artista de samba a ter um especial. </h2>
+            </div>
+        </div>
+    </section>
+
+    <footer style="background-color: #fefefe; color: #00000055; padding: 10px; text-align: center;">
+        <p>2024 Zeca pagodinho. Todos os direitos reservados.</p>
+    </footer>
+
+    <style>
+        .elemento:hover {
+            background-color: #1772b4;
+            transform: scale(1.05);
+            transition: background-color 0.3s ease;
+        }
+        .fade-in {
+            opacity: 0; 
+            transition: opacity 1s ease; 
+          }
+    </style>
+
+</body>
+    `;
+
+    document.body.innerHTML = novoConteudo;
+}
+
+
+
